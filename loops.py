56606745dbae1a4c7d2f3ca36deefabf9456a1a4
Ashwin Deshpande <[ashwindeshpande19@gmail.com]>
2025-03-01 00:47:46 -0500

    while loop

--- loops.py
+++ loops.py
@@ -21,3 +21,23 @@ def char_in_str(string):
 char_in_str(string)
         
         
+"Q1: Write a Python program that takes a number as input and prints all numbers from that number down to 1 using a while loop."
+
+number = 5
+def count_to_one(number):
+    while number >= 1:
+        print(number)
+        number = number - 1
+        
+count_to_one(number)
+
+"Q2: Write a Python program that takes a string as input and prints each character of the string on a new line using a while loop."
+
+strings = "abcdefg"
+def each_char_of_strings(strings):
+    s = 0
+    while s < len(strings):
+        print(strings[s])
+        s = s + 1
+        
+each_char_of_strings(strings)
